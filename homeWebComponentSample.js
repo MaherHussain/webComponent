cf944191cecacc17e00178b2f812f810c1f95622
Maher Hussain <48909747+MaherHussain@users.noreply.github.com>
2024-04-11 09:12:08 +0200

    Add files via upload
    home web component samole

---
+++ homeWebComponentSample.js
@@ -0,0 +1,34 @@
+class HomeComponent extends HTMLElement {
+    static get observedAttributes() {
+        return ['prop']; // Add the prop attribute to the list of observed attributes
+    }
+
+    constructor () {
+        super()
+        this.attachShadow({ mode: 'open' })
+
+        this._prop = this.getAttribute('prop') // Initialize the property from the attribute
+
+        this.container = document.createElement('div')
+
+        this.shadowRoot.appendChild(this.container)
+        this.renderElements()
+    }
+
+    renderElements() {
+        this.container.innerHTML = `<div style="padding: 24px; border: 9px solid blue;">
+                                        <p>page is: ${this._prop ? this._prop : 'not available'}</p>
+                                    </div>`
+    }
+
+    set prop(value) { // Define a setter for the property
+        this._prop = value
+        this.renderElements()
+    }
+
+    get prop() { // Define a getter for the property
+        return this._prop
+    }
+}
+
+customElements.define('home-component', HomeComponent)

bd197176c1eeb2e260109f80c579e53c45d68eb2
Maher Hussain <48909747+MaherHussain@users.noreply.github.com>
2024-05-19 07:01:03 +0200

    Update homeWebComponentSample.js

--- homeWebComponentSample.js
+++ homeWebComponentSample.js
@@ -3,94 +3,86 @@ class CustomCriteriaComponent extends HTMLElement {
         return ['prop']; // Add the prop attribute to the list of observed attributes
     }
 
-    constructor () {
-        super()
-        this.attachShadow({ mode: 'open' })
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
 
-        this._prop = this.getAttribute('prop') // Initialize the property from the attribute
+    this._props = this.getAttribute('props') // Initialize the property from the attribute
 
-        this.container = document.createElement('div')
+    this.container = document.createElement('div')
+    this.container.classList.add('ucommerce-data-requested__container') // Adding a class for styling
 
-        this.shadowRoot.appendChild(this.container)
-        this.renderElements()
-    }
+    this.shadowRoot.appendChild(this.container)
 
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (name === 'header') {
-      this.renderElements();
-    }
+    this.renderElements()
   }
+
+  renderElements() {
+    this.container.innerHTML = `
+      <style>
+        .ucommerce-data-requested__container {
+            display: block;
+            padding: 24px;
+            margin-bottom: 24px;
+            margin-top: 24px;
+            border: 2px solid #e8e8ed;
+            border-radius: 10px;
+            box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+            transition: 0.3s;
+        }
   
-  connectedCallback() {
-        this.renderElements();
-        this.shadowRoot.querySelector('input').addEventListener('input', this.handleInputChange);
-        this.shadowRoot.querySelector('select').addEventListener('change', this.handleDropdownChange);
-      }
-
-  disconnectedCallback() {
-        this.shadowRoot.querySelector('input').removeEventListener('input', this.handleInputChange);
-        this.shadowRoot.querySelector('select').removeEventListener('change', this.handleDropdownChange);
-      }
-
-  handleInputChange(event) {
-        console.log('Input changed:', event.target.value);
-      }
-
-  handleDropdownChange(event) {
-        console.log('Dropdown changed:', event.target.value);
-      }
-
-    renderElements() {
-        this.container.innerHTML = `
-          <style>
-            .container {
-              border: 1px solid #ccc;
-              padding: 16px;
-              max-width: 300px;
-              margin: auto;
-            }
-            .header {
-              font-size: 24px;
+        .ucommerce-data-requested__container:hover {
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        }
+  
+        h2 {
+            color: #b4b4c6;
+            text-align: center;
+        }
+  
+        h5 {
+            margin: 0;
+            color: #5a7179
+        }
+  
+        button {
+              padding: 20px;
+              font-size: 16px;
               font-weight: bold;
-              margin-bottom: 16px;
-            }
-            .form-group {
-              margin-bottom: 16px;
-            }
-            input, select {
-              width: 100%;
-              padding: 8px;
-              margin-top: 4px;
-              box-sizing: border-box;
-            }
-          </style>
-          <div class="container">
-            <h2 class="header">${this._props?.header ? this._props.header : 'Not Available'}</h2>
-            <div class="form-group">
-              <label for="inputField">My custome critria input:</label>
-              <input type="text" id="inputField" />
-            </div>
-            <div class="form-group">
-              <label for="dropdown">My custome critria Dropdown:</label>
-              <select id="dropdown">
-                <option value="option1">Option 1</option>
-                <option value="option2">Option 2</option>
-                <option value="option3">Option 3</option>
-              </select>
-            </div>
-          </div>
-        `;
-    }
+              border-radius: 10px;
+              border: solid 1px #e8e8ed;
+              color: #5a7179;
+              cursor: pointer;
+        }
+  
+        button:hover {
+          box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+        }
+      </style>
+      <div>
+      <h5>Sample of custom web component from Ucommerce</h5>
+          <h2>${this._props?.header || ''}</h2>
+          
+      </div>
+  `
 
-    set prop(value) { // Define a setter for the property
-        console.log(value)
-        this._prop = value
-        this.renderElements()
-    }
+    this.shadowRoot
+      .getElementById('logbook-loader-button')
+      .addEventListener('click', () =>
+        this.handleClick(this._props.orderGuid, this._props.cultureCode)
+      )
+  }
 
-    get prop() { // Define a getter for the property
-        return this._prop
-    }
+  set props(value) {
+    // Define a setter for the property
+    this._props = value
+    this.renderElements()
+  }
+
+  get props() {
+    // Define a getter for the property
+    return this._props
+  }
 }
 
 customElements.define('custom-criteria-component', CustomCriteriaComponent)
